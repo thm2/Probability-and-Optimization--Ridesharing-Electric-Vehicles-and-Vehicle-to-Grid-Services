0a20e0b6b5489d1dd53591053ad679d0f5550dd7
thm2 <98324965+thm2@users.noreply.github.com>
2022-12-18 00:19:06 +0200

    Update maxrAvsC.m

--- Ridesharing Code/maxrAvsC.m
+++ Ridesharing Code/maxrAvsC.m
@@ -20,7 +20,6 @@ ag=2; bg=1; E=ag*bg;
 % axes - first max
 p1_start=0.01; dt1=0.05; p1_end=maxcfp1*pmax-dt1; p1=linspace(p1_start,p1_end,(p1_end-p1_start)/dt1);
 p2_start=0.01; dt2=0.05; p2_end=maxcfp2*pmax; p2=linspace(p2_start,p2_end,(p2_end-p2_start)/dt2);
-% l_start=1; dt3=20; l_end=700; lambda=linspace(l_start,l_end,(l_end-l_start)/dt3);
 c=[1;5;10;15;20;25;30;35;40;45;60;120];
 
 % arrival rates
@@ -34,7 +33,6 @@ RMAX=[]; P22=[]; P11=[];
 for lambda=8:2:12
     RAMAX1=[]; ERR=[]; 
 lamm=0;  rA1=zeros(length(p1),length(p2),length(c));
-% for c_incr=1:length(c) %
 for c_incr=1:length(c)
     lamm=lamm+1;
     
@@ -46,15 +44,14 @@ for c_incr=1:length(c)
         end
         max(ERR);
     end
-%     RAMAX11=max(max(rA1(:,:,lamm)));
-%     RAMAX1=[RAMAX1;max(max(RAMAX11))];
 end
 
 
 
 
 LLL=[]; LINEARINDEX=[]; P1=[]; P2=[];
-% here it extracts the optimal prices from TotExpProfitRate
+
+% extract the optimal prices from TotExpProfitRate
 for LL=1:size(rA1,3);
     [maxValue, linearIndexesOfMaxes] = max(max(rA1(:,:,LL)));
     if min(min(isnan((rA1(:,:,LL)))))~=1 % only the non nan exp. rates (they have at least one max) are kept
@@ -89,7 +86,7 @@ else
 end
 
 
-% gradient ascend
+%%% gradient ascend
 
 % gamma cdf and derivative
 F=@(p1)gamcdf(p1,ag,bg); dF=@(p1)p1^(ag-1)*exp(-p1/bg)/(gamma(ag)*bg^ag);
@@ -141,17 +138,6 @@ P22=[P22;P2]
 end
 
 
-
-% a = gca;
-% a.TickLabelInterpreter = 'latex';
-% a.Box = 'on';
-% a.BoxStyle = 'full';
-
-% scatter(c,P1,[],rA1(LINEARINDEX),'filled')
-% hold on
-% scatter(c,P2,[],rA1(LINEARINDEX),'LineWidth',1.8) % ,'MarkerEdgeColor',[0 .5 .5],
-
-
 figure
 plot(c,P22(1:length(c))./P11(1:length(c)),'r','LineWidth',1.8)
 hold on
@@ -159,33 +145,26 @@ plot(c,P22(1:length(c))./P11(1:length(c)),'r*','LineWidth',1.8)
 hold on
 plot(c,P22(length(c)+1:2*length(c))./P11(length(c)+1:2*length(c)),'b','LineWidth',1.8)
 hold on
-% plot(c,P22(length(c)+1:2*length(c))./P11(length(c)+1:2*length(c)),'b+','LineWidth',1.8)
-% hold on
 plot(c,P22(2*length(c)+1:3*length(c))./P11(2*length(c)+1:3*length(c)),'k','LineWidth',1.8)
 hold on
-% plot(c,P22(3*length(c)+1:4*length(c))./P11(3*length(c)+1:4*length(c)),'LineWidth',1.8)
 xlabel('$c$', 'Interpreter', 'Latex', 'Fontsize', 20)
 ylabel('${p_2^*}/{p_1^*}$', 'Interpreter', 'Latex', 'Fontsize', 20),
 title('Optimal price ratio versus $c$', 'Interpreter', 'Latex', 'Fontsize', 17)
-% leg = legend(gca, 'show'), set(leg, 'fontsize',18, 'Interpreter','latex')
 grid on
 saveas(gcf,'../Figures/maxPriceRatio_vs_c.png')
 
 
 
 figure
-% plot(c,RMAX(1:length(c)),c,RMAX(length(c)+1:2*length(c)),c,RMAX(2*length(c)+1:3*length(c)),'LineWidth',3,strcat('$\lambda =$',num2str(8)))
 plot(c,RMAX(1:length(c)),'r','LineWidth',3,'DisplayName',strcat('$\lambda =$',num2str(8)))
 hold on
 plot(c,RMAX(length(c)+1:2*length(c)),'b','LineWidth',3, 'DisplayName', strcat('$\lambda =$',num2str(10)))
 hold on
 plot(c,RMAX(2*length(c)+1:3*length(c)),'k','LineWidth',3,'DisplayName',strcat('$\lambda =$',num2str(12)))
 hold on
-% plot(c,RMAX(3*length(c)+1:4*length(c)),'LineWidth',3)
 xlabel('$c$', 'Interpreter', 'Latex', 'Fontsize', 30)
 ylabel('$r_{\mathbf A}^*$', 'Interpreter', 'Latex', 'Fontsize', 30),
 title('Optimal $r_{\bf A}$ versus $c$', 'Interpreter', 'Latex', 'Fontsize', 24)
-% leg = legend(gca, 'show'), set(leg, 'fontsize',18, 'Interpreter','latex')
 grid on
 
 saveas(gcf,'../Figures/maxrA_vs_c.png')
